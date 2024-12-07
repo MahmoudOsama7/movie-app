e476a607afc31192818b9c8b128cb460cc16819b
mahmoud <mahmoud@34ml.com>
2024-12-08 01:47:57 +0200

    organizing files
    1-creating movie data module for hosting all project data

---
+++ movie-data/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.example.movie_data"
+    compileSdk = libs.versions.compileSDK.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSDK.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtensionVersion.get()
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.jvmTarget.get()
+    }
+}
+
+dependencies {
+    implementation(projects.network)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.bundles.hilt)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.coil)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.pagination)
+    kapt(libs.room.compiler)
+}
